afd88c3108193cbb12c1db079eac4230d7dfff00
vjnambi <vjnambi@gmail.com>
2023-04-01 11:33:38 -0700

    April fools day front end

--- src/components/DisplayTimer.js
+++ src/components/DisplayTimer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function DisplayTimer({gameState}) {
+  if(gameState && gameState.currentQuestionNum >= 2 && (gameState.status === "question" || gameState.status === "answer")){
+    return <div>APRIL FOOLS!</div>
+  }
   if(gameState && (gameState.status === "question" || gameState.status === "answer")){
     return <div>{gameState.timeKeeper.time}</div>
   }
--- src/components/SubmitCreateGame.js
+++ src/components/SubmitCreateGame.js
@@ -11,73 +11,9 @@ function SubmitCreateGame({gameId, setGameId}) {
         const body = {
             "payload": [
                 {
-                    "url": "HZvnP0svJzk",
-                    "answer": "A Dream That is More Scarlet Than Red"
+                    "url": "dQw4w9WgXcQ",
+                    "answer": "Never Gonna Give You Up"
                 },
-                {
-                    "url": "w279PjazOAw",
-                    "answer": "A Soul as Red as a Ground Cherry"
-                },
-                {
-                    "url": "zUvEHNW6y_A",
-                    "answer": "Apparitions Stalk the Night"
-                },
-                {
-                    "url": "-Gx6D-i2Fqo",
-                    "answer": "Lunate Elf"
-                },
-                {
-                    "url": "FG7flaqNFIY",
-                    "answer": "Tomboyish Girl in Love"
-                },
-                {
-                    "url": "XpPlPKWW1Y0",
-                    "answer": "Shanghai Teahouse ~ Chinese Tea"
-                },
-                {
-                    "url": "qefeBGGszRI",
-                    "answer": "Shanghai Alice of Meiji 17"
-                },
-                {
-                    "url": "ogtZZLPHqus",
-                    "answer": "Voile, the Magic Library"
-                },
-                {
-                    "url": "qUEPCSzOHmo",
-                    "answer": "Locked Girl ~ the Girl's Secret Room"
-                },
-                {
-                    "url": "cBWOIyKZ6is",
-                    "answer": "The Maid and the Pocket Watch of Blood"
-                },
-                {
-                    "url": "m1nRuWqW09Q",
-                    "answer": "Lunar Clock ~ Luna Dial"
-                },
-                {
-                    "url": "JZ1jgSkP0Oc",
-                    "answer": "The Young Descendant of Tepes"
-                },
-                {
-                    "url": "uQ1YJMvSKP8",
-                    "answer": "Septette for the Dead Princess"
-                },
-                {
-                    "url": "B_5o9nETAPE",
-                    "answer": "The Centennial Festival for Magical Girls"
-                },
-                {
-                    "url": "xcvouNwZbI0",
-                    "answer": "U.N. Owen was Her?"
-                },
-                {
-                    "url": "OQ5f49lDH7E",
-                    "answer": "An Eternity that is More Transient than Scarlet"
-                },
-                {
-                    "url": "xtv5YP_Hgzs",
-                    "answer": "Crimson Tower ~ Eastern Dream"
-                }
             ]
         };
         axios.post(`https://thrensmusicquizapi-thrensmusicquizapi.azuremicroservices.io/addQuestionSet/${temp}`, body)
