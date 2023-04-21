dd26f6edeaa7f89122ee43fb09417f4b06c4aa22
vjnambi <vjnambi@gmail.com>
2023-04-21 13:15:09 -0500

    configure BEDomain to api

--- src/lib/BEDomain.js
+++ src/lib/BEDomain.js
@@ -1,5 +1,6 @@
 //var BEDomain = "http://localhost:8080"
 //var BEDomain = "https://thrensmusicquizapi-thrensmusicquizapi.azuremicroservices.io"
-var BEDomain = "http://tmq-aci.eastus.azurecontainer.io:8080"
+//var BEDomain = "http://tmq-aci.eastus.azurecontainer.io:8080"
+var BEDomain = "https://tmq-apim.azure-api.net/tmqapi"
 
 export default BEDomain
