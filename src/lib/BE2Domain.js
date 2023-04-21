e8f436ba2a1b15ad8985d408b7bb0055e1a50860
vjnambi <vjnambi@gmail.com>
2023-04-21 13:19:53 -0500

    configure other domains

--- src/lib/BE2Domain.js
+++ src/lib/BE2Domain.js
@@ -1,4 +1,4 @@
-var BE2Domain = "http://localhost:3001"
-//var BE2Domain = "https://thrensmusicquizapi.azurewebsites.net"
+//var BE2Domain = "http://localhost:3001"
+var BE2Domain = "https://thrensmusicquizapi.azurewebsites.net"
 
 export default BE2Domain
