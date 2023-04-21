9baca94eb833b875704174e07e454120e8bd7030
vjnambi <vjnambi@gmail.com>
2023-04-21 14:35:10 -0500

    changed BEDomain to use app service

--- src/lib/BEDomain.js
+++ src/lib/BEDomain.js
@@ -1,6 +1,7 @@
 //var BEDomain = "http://localhost:8080"
 //var BEDomain = "https://thrensmusicquizapi-thrensmusicquizapi.azuremicroservices.io"
 //var BEDomain = "http://tmq-aci.eastus.azurecontainer.io:8080"
-var BEDomain = "https://tmq-apim.azure-api.net/tmqapi"
+//var BEDomain = "https://tmq-apim.azure-api.net/tmqapi"
+var BEDomain = "https://thrensmusicquizapi-1682105183110.azurewebsites.net"
 
 export default BEDomain
